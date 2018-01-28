0fbab2743bff4b6074abc829427a1df059396724
ISM <ismail@example.com>
2018-01-28 12:57:20 +0200

    Add Password Templates

--- base/auth/passwords/email.blade.php
+++ base/auth/passwords/email.blade.php
@@ -1,47 +1,27 @@
-@extends('backpack::layout')
+@extends('backpack::auth_layout')
 
-<!-- Main Content -->
 @section('content')
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="box box-default">
-                <div class="box-header with-border">
-                    <div class="box-title">{{ trans('backpack::base.reset_password') }}</div>
-                </div>
-                <div class="box-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('backpack.auth.password.email') }}">
-                        {!! csrf_field() !!}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">{{ trans('backpack::base.email_address') }}</label>
-
-                            <div class="col-md-6">
-                                <input type="email" class="form-control" name="email" value="{{ old('email') }}">
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-envelope"></i> {{ trans('backpack::base.send_reset_link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+    @if (session('status'))
+        <div class="alert alert-success">
+            {{ session('status') }}
         </div>
-    </div>
+    @endif
+    <form action="{{ route('backpack.auth.password.email') }}" method="post">
+        {!! csrf_field() !!}
+        <h3 class="font-green">{{ trans('backpack::base.reset_password') }}</h3>
+        <div class="form-group {{ $errors->has('email') ? ' has-error' : '' }}">
+            <input type="email" class="form-control placeholder-no-fix"
+                   placeholder="{{ trans('backpack::base.email_address') }}" name="email" value="{{ old('email') }}">
+            @if ($errors->has('email'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('email') }}</strong>
+                </span>
+            @endif
+        </div>
+        <div class="form-actions">
+            <button type="submit" class="btn btn-success uppercase pull-right">
+                <i class="fa fa-btn fa-envelope"></i> {{ trans('backpack::base.send_reset_link') }}
+            </button>
+        </div>
+    </form>
 @endsection
